6ee0fea2b464254802273500d895ee6fd5175b58
bobrossiotta <137832556+bobrossiotta@users.noreply.github.com>
2023-07-05 21:46:16 +0300

    added all files

---
+++ Test.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * by bobrossiotta
+         */
+        // MONTH - HOROSCOPE
+        // String
+        String month,january,february,march,april,may,june,july,august,september,october,november,december;
+        String day;
+        Scanner bob = new Scanner(System.in);
+        System.out.println("Doğduğunuz ay: ");
+        month = bob.nextLine();
+        System.out.println("Doğduğunuz gün: ");
+        day=bob.nextLine();
+        if (month.equals("January")&& (day>=19&&day<=22));
+
+
+
+
+
+    } }
